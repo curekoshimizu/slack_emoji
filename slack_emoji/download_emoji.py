3af2dae2657e7d3e7154dfa4eb731f298f93acf8
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-03-22 22:01:55 +0900

    download parrot

--- slack_emoji/download_emoji.py
+++ slack_emoji/download_emoji.py
@@ -1,5 +1,7 @@
 import asyncio
+import os
 import pathlib
+import zipfile
 from typing import Dict
 
 import aiohttp
@@ -7,14 +9,21 @@ import yaml
 
 
 async def download_emoji(dest_dir: pathlib.Path, registed_emoji_dict: Dict[str, str]) -> None:
-    emoji_dict = get_emoji_dict()
-
     limit = 128
     sem = asyncio.Semaphore(limit)
 
+    await get_slack_emoji_from_asserts_dir(sem, dest_dir, registed_emoji_dict)
+    await get_party_parrot(sem, dest_dir, registed_emoji_dict)
+
+
+async def get_slack_emoji_from_asserts_dir(
+    sem: asyncio.Semaphore, dest_dir: pathlib.Path, registed_emoji_dict: Dict[str, str]
+) -> None:
+    emoji_dict = get_emoji_dict()
+
     coros = []
     for name, uri in emoji_dict.items():
-        if name in registed_emoji_dict:
+        if name.split("/")[1] in registed_emoji_dict:
             print(f"{name} is already registered")
             continue
 
@@ -27,13 +36,35 @@ async def download_emoji(dest_dir: pathlib.Path, registed_emoji_dict: Dict[str, 
     await asyncio.gather(*coros)
 
 
+async def get_party_parrot(sem: asyncio.Semaphore, dest_dir: pathlib.Path, registed_emoji_dict: Dict[str, str]) -> None:
+    uri = "https://cultofthepartyparrot.com/parrots-56e80e9d0e.zip"
+    target = dest_dir / "parrot.zip"
+    await download_single_file(sem, target, uri)
+    with zipfile.ZipFile(target) as existing_zip:
+        existing_zip.extractall(dest_dir)
+
+    dirs = ((dest_dir / "parrots"), (dest_dir / "parrots" / "hd"))
+    for d in dirs:
+        assert d.exists(), f"{d} not found..."
+        for fname in d.iterdir():
+            if not fname.is_file():
+                continue
+            name = fname.stem
+            if name in registed_emoji_dict:
+                print(f"{name} is already registered")
+                os.remove(fname.absolute())
+
+
 def get_emoji_dict() -> Dict[str, str]:
     assets_dir = pathlib.Path(__file__).absolute().parents[1] / "assets" / "emojipacks" / "packs"
     assert assets_dir.exists()
 
+    white_list = ["slackmojis-logo.yaml", "octicons.yaml", "nekoatsume.yaml", "frontend.yaml"]
+
     emoji_dict = {}
 
-    for fname in assets_dir.iterdir():
+    for name in white_list:
+        fname = assets_dir / name
         with open(fname, "r") as f:
             config = yaml.safe_load(f)
             for emoji in config["emojis"]:
