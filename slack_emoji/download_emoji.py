15dd9c779c5b61100c8a7ad470d9f858e782ddb0
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-03-22 22:01:31 +0900

    add download API

---
+++ slack_emoji/download_emoji.py
@@ -0,0 +1,56 @@
+import pathlib
+import asyncio
+import aiohttp
+import yaml
+
+
+async def download_emoji(dest_dir: pathlib.Path):
+    emoji_dict = get_emoji_dict()
+
+
+    limit = 128
+    sem = asyncio.Semaphore(limit)
+
+    coros = []
+    for name, uri in emoji_dict.items():
+        target = dest_dir / f"{name}{pathlib.Path(uri).suffix}"
+        if not target.parent.exists():
+            target.parent.mkdir(parents=True)
+
+        coros.append(download_single_file(sem, target, uri))
+
+    await asyncio.gather(*coros)
+
+
+
+
+
+def get_emoji_dict():
+    assets_dir = pathlib.Path(__file__).absolute().parents[1] / "assets" / "emojipacks" / "packs"
+    assert assets_dir.exists()
+
+    emoji_dict = {}
+
+    for fname in assets_dir.iterdir():
+        with open(fname, "r") as f:
+            config = yaml.safe_load(f)
+            for emoji in  config["emojis"]:
+                key = pathlib.Path(pathlib.Path( fname.name).stem) / str(emoji["name"])
+                emoji_dict[ str(key) ] = emoji["src"]
+
+    return emoji_dict
+
+
+async def download_single_file(
+    sem: asyncio.Semaphore,
+    dest: pathlib.Path,
+    uri: str,
+):
+    async with sem:
+        print(dest, "is running")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(uri) as resp:
+                if resp.status == 200:
+                    with open(dest, "wb") as f:
+                        f.write(await resp.read())
+                        f.close()
